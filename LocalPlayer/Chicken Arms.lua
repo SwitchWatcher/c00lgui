c64d951999928e5273862b137580a619a8b37255
/ SlashCoders <156876187+SwitchWatcher@users.noreply.github.com>
2025-02-15 11:57:18 +0800

    <Archived by SlashCoders>

---
+++ LocalPlayer/Chicken Arms.lua
@@ -0,0 +1,4 @@
+game:GetService("Workspace")[game:GetService("Players").LocalPlayer.Name].Torso["Left Shoulder"].C0 = CFrame.new(-1.5, 0.5, 0) * CFrame.fromEulerAnglesXYZ(0,math.pi/2,0) * CFrame.fromEulerAnglesXYZ(math.pi/2, 0, 0) * CFrame.fromEulerAnglesXYZ(0,-math.pi/2,0)
+game:GetService("Workspace")[game:GetService("Players").LocalPlayer.Name].Torso["Left Shoulder"].C1 = CFrame.new(0, 0.5, 0)
+game:GetService("Workspace")[game:GetService("Players").LocalPlayer.Name].Torso["Right Shoulder"].C0 = CFrame.new(1.5, 0.5, 0) * CFrame.fromEulerAnglesXYZ(0,-math.pi/2,0) * CFrame.fromEulerAnglesXYZ(math.pi/2, 0, 0) * CFrame.fromEulerAnglesXYZ(0,-math.pi/2,0)
+game:GetService("Workspace")[game:GetService("Players").LocalPlayer.Name].Torso["Right Shoulder"].C1 = CFrame.new(0, 0.5, 0)
